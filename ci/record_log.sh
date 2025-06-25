9bbb9580361f4136aab2f7d7cf7eb57fafb8dcb3
justincdavis <davisjustin302@gmail.com>
2025-06-25 10:35:02 -0500

    redo how logs are recorded, save all and basic versions, requires setting up tegrastats with global sudo privileges

--- ci/record_log.sh
+++ ci/record_log.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-python -m jetsontools profile --output data/logs/$1.tegrastats --interval 5 --readall --sudo --command sleep 1
+mkdir -p data/logs/$1
+python3 -m jetsontools profile --output data/logs/$1/all.tegrastats --interval 5 --readall --sudo --command sleep 1
+python3 -m jetsontools profile --output data/logs/$1/basic.tegrastats --interval 5 --command sleep 1
