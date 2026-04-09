48cb86923e9bc4c792d8e055d0152863cecde3ab
justincdavis <davisjustin302@gmail.com>
2026-04-09 16:00:49 -0500

    Bump version 0.1.3 -> 0.1.4

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import jetsontools
 project = 'jetsontools'
 copyright = '2024, Justin Davis'
 author = 'Justin Davis'
-version = '0.1.3'
+version = '0.1.4'
 
 assert version == jetsontools.__version__
 
